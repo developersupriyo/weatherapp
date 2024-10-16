b896b1ff8cac042a2962f46a191e21a20217892a
Smalakar <73283119+malakarsupriyo@users.noreply.github.com>
2024-10-16 11:35:26 +0530

    Live Location Updated script.js

--- script.js
+++ script.js
@@ -46,3 +46,33 @@ input.addEventListener("keypress", function(event) {
   }
 });
 }
+
+
+// -------------------Current Location Button--------------------
+
+{const locationButton = document.querySelector(".location-btn");
+const API_KEY = "c38e0279b7bc85480c3fe775d518c";
+
+const getUserCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            const { latitude, longitude } = position.coords; // Get coordinates of user location
+            // Get city name from coordinates using reverse geocoding API
+            const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            fetch(API_URL).then(response => response.json()).then(data => {
+                const { name } = data[0];
+                getWeatherDetails(name, latitude, longitude);
+            }).catch(() => {
+                alert("An error occurred while fetching the city name!");
+            });
+        },
+        error => { // Show alert if user denied the location permission
+            if (error.code === error.PERMISSION_DENIED) {
+                alert("Geolocation request denied. Please reset location permission to grant access again.");
+            } else {
+                alert("Geolocation request error. Please reset location permission.");
+            }
+        });
+}
+locationButton.addEventListener("click", getUserCoordinates);
+}
